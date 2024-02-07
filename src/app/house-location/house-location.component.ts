9743386c43d0871a69743d4ddfd4946b2f11574f
Arnaud <arnaud.chane.dev@gmail.com>
2024-02-07 13:49:02 +0100

    Gen house location component

---
+++ src/app/house-location/house-location.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-house-location',
+  standalone: true,
+  imports: [],
+  templateUrl: './house-location.component.html',
+  styleUrl: './house-location.component.scss'
+})
+export class HouseLocationComponent {
+
+}
