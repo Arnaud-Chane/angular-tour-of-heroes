05272c06273fc2ed6df56deecdc6bb126fc98fca
Arnaud <arnaud.chane.dev@gmail.com>
2024-02-07 11:57:48 +0100

    Adding vilains component

---
+++ src/app/vilains/vilains.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vilains',
+  standalone: true,
+  imports: [],
+  templateUrl: './vilains.component.html',
+  styleUrl: './vilains.component.scss'
+})
+export class VilainsComponent {
+
+}
