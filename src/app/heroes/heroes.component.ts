a3b4b6e3c9b2b4ee92aee628c6fa1b9f62eb6234
Arnaud <arnaud.chane.dev@gmail.com>
2024-02-07 11:55:54 +0100

    Adding messageService to hero Component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,26 +1,37 @@
-import { Component, Input, Output, OnInit, EventEmitter, Inject } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  OnInit,
+  EventEmitter,
+  Inject,
+} from '@angular/core';
+import { MessageService } from '../services/message.service';
 
 @Component({
   standalone: true,
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss']
+  styleUrls: ['./heroes.component.scss'],
 })
 export class HeroesComponent implements OnInit {
-  hero: string = "Windstorm";
+  hero: string = 'Windstorm';
 
-  @Input() inputFromParent: string = "";
-  ngOnInit(): void {
-  }
+  @Input() inputFromParent: string = '';
+  ngOnInit(): void {}
 
-  textToParent: string = "This is for parent from child !";
-  @Output() updatedText = new EventEmitter <string> ();
+  textToParent: string = 'This is for parent from child !';
+  textThroughtService: string = "Message throught service";
+  @Output() updatedText = new EventEmitter<string>();
 
-  // constructor(textToParent: string) { 
+  // constructor(textToParent: string) {
   //   this.textToParent = textToParent;
   // }
 
+  constructor(private messageService: MessageService){}
+
   PostData() {
     this.updatedText.emit(this.textToParent);
+    this.messageService.sendData(this.textThroughtService);
   }
 }
